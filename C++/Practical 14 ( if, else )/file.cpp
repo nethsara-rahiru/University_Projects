015115110c87a52a0b9596cf6d8a5dae9425f4da
nethsara-rahiru <rahiru123@gmail.com>
2024-10-28 16:35:38 +0530

    Updated a new practical ( if, else )

---
+++ C++/Practical 14 ( if, else )/file.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int number = 5;
+
+    if(number > 0)
+    {
+        cout << "The number is bigger than 0";
+    }
+    else
+    {
+        cout << "The number is smaller than 0";
+    }
+}
